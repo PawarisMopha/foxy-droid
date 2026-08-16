7d5a88523eeaf726f5ba87dc2eebcc2076fdfd90
Pawaris Mopha <251288879+PawarisMopha@users.noreply.github.com>
2026-08-17 03:35:19 +0700

    Refactor RxUtils for improved readability and structure

--- src/main/kotlin/nya/kitsunyan/foxydroid/utility/RxUtils.kt
+++ src/main/kotlin/nya/kitsunyan/foxydroid/utility/RxUtils.kt
@@ -11,73 +11,73 @@ import okhttp3.Call
 import okhttp3.Response
 
 object RxUtils {
-    private class ManagedDisposable(private val cancel: () -> Unit) : Disposable {
-        @Volatile var disposed = false
-        override fun isDisposed(): Boolean = disposed
+  private class ManagedDisposable(private val cancel: () -> Unit): Disposable {
+    @Volatile var disposed = false
+    override fun isDisposed(): Boolean = disposed
 
-        override fun dispose() {
-            disposed = true
-            cancel()
+    override fun dispose() {
+      disposed = true
+      cancel()
+    }
+  }
+
+  private fun <T : Any, R : Any> managedSingle(create: () -> T, cancel: (T) -> Unit, execute: (T) -> R): Single<R> {
+    return Single.create {
+      val task = create()
+      val thread = Thread.currentThread()
+      val disposable = ManagedDisposable {
+        thread.interrupt()
+        cancel(task)
+      }
+      it.setDisposable(disposable)
+      if (!disposable.isDisposed) {
+        val result = try {
+          execute(task)
+        } catch (e: Throwable) {
+          Exceptions.throwIfFatal(e)
+          if (!disposable.isDisposed) {
+            try {
+              it.onError(e)
+            } catch (inner: Throwable) {
+              Exceptions.throwIfFatal(inner)
+              RxJavaPlugins.onError(CompositeException(e, inner))
+            }
+          }
+          null
         }
-    }
-
-    private fun <T : Any, R : Any> managedSingle(create: () -> T, cancel: (T) -> Unit, execute: (T) -> R): Single<R> {
-        return Single.create {
-            val task = create()
-            val thread = Thread.currentThread()
-            val disposable = ManagedDisposable {
-                thread.interrupt()
-                cancel(task)
-            }
-            it.setDisposable(disposable)
-            if (!disposable.isDisposed) {
-                val result = try {
-                    execute(task)
-                } catch (e: Throwable) {
-                    Exceptions.throwIfFatal(e)
-                    if (!disposable.isDisposed) {
-                        try {
-                            it.onError(e)
-                        } catch (inner: Throwable) {
-                            Exceptions.throwIfFatal(inner)
-                            RxJavaPlugins.onError(CompositeException(e, inner))
-                        }
-                    }
-                    null
-                }
-                if (result != null && !disposable.isDisposed) {
-                    it.onSuccess(result)
-                }
-            }
+        if (result != null && !disposable.isDisposed) {
+          it.onSuccess(result)
         }
+      }
     }
+  }
 
-    fun <R : Any> managedSingle(execute: () -> R): Single<R> {
-        return managedSingle({ Unit }, {}, { execute() })
-    }
+  fun <R> managedSingle(execute: () -> R): Single<R> {
+    return managedSingle({ Unit }, { }, { execute() })
+  }
 
-    fun callSingle(create: () -> Call): Single<Response> {
-        return managedSingle(create, Call::cancel, Call::execute)
-    }
+  fun callSingle(create: () -> Call): Single<Response> {
+    return managedSingle(create, Call::cancel, Call::execute)
+  }
 
-    fun <T : Any> querySingle(query: (CancellationSignal) -> T): Single<T> {
-        return Single.create {
-            val cancellationSignal = CancellationSignal()
-            it.setCancellationSignal {
-                try {
-                    cancellationSignal.cancel()
-                } catch (e: OperationCanceledException) {
-                    // Do nothing
-                }
-            }
-            val result = try {
-                query(cancellationSignal)
-            } catch (e: OperationCanceledException) {
-                null
-            }
-            if (result != null) {
-                it.onSuccess(result)
-            }
+  fun <T> querySingle(query: (CancellationSignal) -> T): Single<T> {
+    return Single.create {
+      val cancellationSignal = CancellationSignal()
+      it.setCancellable {
+        try {
+          cancellationSignal.cancel()
+        } catch (e: OperationCanceledException) {
+          // Do nothing
         }
+      }
+      val result = try {
+        query(cancellationSignal)
+      } catch (e: OperationCanceledException) {
+        null
+      }
+      if (result != null) {
+        it.onSuccess(result)
+      }
     }
+  }
 }
